327ba556d15a7188471d9508f1d351cca8907723
tonigl <toni.smith.86@hotmail.com>
2014-09-11 14:48:10 +0200

    Afegit layout menu principal, afegit split action bar en android manifest, comprovar que funcioni.

--- app/src/main/java/etp/com/elteuparlament/MyActivity.java
+++ app/src/main/java/etp/com/elteuparlament/MyActivity.java
@@ -12,7 +12,7 @@ public class MyActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my);
+        setContentView(R.layout.activity_menu_main);
     }
 
 
