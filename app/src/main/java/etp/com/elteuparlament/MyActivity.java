49adf4aed314d0aa6f2db6c5d8d8cc980ec1a4f4
tonigl <toni.smith.86@hotmail.com>
2014-09-13 13:28:23 +0200

    Fixed overlay actionbar, file strings and splitActionBar

--- app/src/main/java/etp/com/elteuparlament/MyActivity.java
+++ app/src/main/java/etp/com/elteuparlament/MyActivity.java
@@ -2,6 +2,8 @@ package etp.com.elteuparlament;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 //a
 
@@ -12,6 +14,14 @@ public class MyActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.my, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
 
